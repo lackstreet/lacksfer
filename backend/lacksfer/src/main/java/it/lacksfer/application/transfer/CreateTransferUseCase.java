7e65a146e59f2753fd19fac618ce280a2c9a0e6d
LackStreet <abrefaemmanuel@gmail.com>
2026-05-03 16:16:59 +0200

    chore: add transfer creation logging

--- backend/lacksfer/src/main/java/it/lacksfer/application/transfer/CreateTransferUseCase.java
+++ backend/lacksfer/src/main/java/it/lacksfer/application/transfer/CreateTransferUseCase.java
@@ -3,6 +3,7 @@ package it.lacksfer.application.transfer;
 import it.lacksfer.domain.Transfer;
 import it.lacksfer.ports.out.TransferRepositoryPort;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.logging.Logger;
 
 
 import java.time.Instant;
@@ -11,13 +12,20 @@ import java.time.Instant;
 public class CreateTransferUseCase {
 
     private final TransferRepositoryPort repository;
+    private static final Logger LOG = Logger.getLogger(CreateTransferUseCase.class);
 
     public CreateTransferUseCase(TransferRepositoryPort repository){
         this.repository = repository;
     }
 
     public Transfer execute(String fileName, Instant expiresAt){
+        LOG.infof("Creating transfer for fileName=%s", fileName);
+
         Transfer transfer = Transfer.createNew(fileName, expiresAt);
-        return repository.save(transfer);
+        Transfer saved = repository.save(transfer);
+
+        LOG.infof("Transfer created id=%s",saved.getId());
+
+        return saved;
     }
 }
--- backend/lacksfer/src/main/java/it/lacksfer/application/transfer/GetTransferByDownloadTokenUseCase.java
+++ backend/lacksfer/src/main/java/it/lacksfer/application/transfer/GetTransferByDownloadTokenUseCase.java
@@ -3,18 +3,21 @@ package it.lacksfer.application.transfer;
 import it.lacksfer.domain.Transfer;
 import it.lacksfer.ports.out.TransferRepositoryPort;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.logging.Logger;
 
 import java.util.Optional;
 
 @ApplicationScoped
 public class GetTransferByDownloadTokenUseCase {
     private final TransferRepositoryPort transferRepositoryPort;
-
+    private static final Logger LOG = Logger.getLogger(GetTransferByDownloadTokenUseCase.class);
     public GetTransferByDownloadTokenUseCase(TransferRepositoryPort transferRepositoryPort) {
         this.transferRepositoryPort = transferRepositoryPort;
     }
 
     public Optional<Transfer> execute(String downloadToken) {
+        LOG.infof("Search for downloadToken=%s", downloadToken);
+
         if (downloadToken == null || downloadToken.isBlank()) {
             throw new IllegalArgumentException("downloadToken cannot be null or blank");
         }
