d511cf3f1763a7081300990617e3bf949cd264cc
LackStreet <abrefaemmanuel@gmail.com>
2026-05-10 15:22:58 +0200

    feat: "refactor rest exception mappers"

--- backend/lacksfer/src/main/java/it/lacksfer/adapters/in/rest/exception/mapper/ValidationExceptionMapper.java
+++ backend/lacksfer/src/main/java/it/lacksfer/adapters/in/rest/exception/mapper/ValidationExceptionMapper.java
@@ -1,17 +1,19 @@
-package it.lacksfer.adapters.in.rest.exception;
+package it.lacksfer.adapters.in.rest.exception.mapper;
 
 import it.lacksfer.adapters.in.rest.dto.ValidationErrorResponse;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 import java.util.List;
 @Provider
-public class ValidationExceptionHandler implements ExceptionMapper<ConstraintViolationException> {
+public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
     @Override
     public Response toResponse(ConstraintViolationException exception) {
         List<String> errors = exception.getConstraintViolations().stream().map(v -> v.getMessage()).toList();
-        return Response.status(Response.Status.BAD_REQUEST).entity(new ValidationErrorResponse("Validation failed", errors)).build();
+        return Response.status(Response.Status.BAD_REQUEST).entity(new ValidationErrorResponse("Validation failed", errors))
+                .type(MediaType.APPLICATION_JSON_TYPE).build();
     }
 }
