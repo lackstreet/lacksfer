dda80d30def4927df6d5fb3a9c786b7fb36ca659
LackStreet <abrefaemmanuel@gmail.com>
2026-05-31 17:27:10 +0200

    feat: "feat: add block upload storage calls"

--- frontend/lacksfer-web/src/app/features/transfers/services/transfer-api.service.ts
+++ frontend/lacksfer-web/src/app/features/transfers/services/transfer-api.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpEvent, HttpClient } from '@angular/common/http';
 import { UploadTransferResponse, CompleteTransferUploadResponse, StartDirectUploadResponse } from '../models/transfer.models';
 import { Observable } from 'rxjs';
+import { createBlockId } from '../utils/block-id';
 
 @Injectable({
   providedIn: 'root',
@@ -36,5 +37,30 @@ export class TransferApiService {
 
   completeDirectUpload(transferId: string): Observable<CompleteTransferUploadResponse> {
     return this.http.post<CompleteTransferUploadResponse>(`/api/transfers/${transferId}/complete`, {});
+  }
+
+  uploadBlock(uploadUrl: string, blockIndex: number, block: Blob): Observable<HttpEvent<void>> {
+    const blockId = encodeURIComponent(createBlockId(blockIndex));
+    const blockUrl = `${uploadUrl}&comp=block&blockid=${blockId}`;
+
+    return this.http.put<void>(blockUrl, block, {
+      observe: 'events',
+      reportProgress: true,
+    });
+  }
+
+  commitBlockList(uploadUrl: string, blockIndexes: number[]): Observable<void> {
+    const blockListUrl = `${uploadUrl}&comp=blocklist`;
+
+    const blockListXml = `<?xml version="1.0" encoding="utf-8"?>
+    <BlockList>
+    ${blockIndexes.map((index) => `  <Latest>${createBlockId(index)}</Latest>`).join('\n')}
+    </BlockList>`;
+
+    return this.http.put<void>(blockListUrl, blockListXml, {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    });
   }
 }
